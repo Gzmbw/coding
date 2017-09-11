9c4ee145a65251087b3e4b2015e5ee3248b6fe78
Gzmbw <unbroken1127@gmail.com>
2017-09-11 10:24:29 +0800

    练习

---
+++ imooc/cpp/cpp继承篇/3继承方式/巩固练习.cpp
@@ -0,0 +1,56 @@
+//定义两个类，人类中含有数据成员姓名（m_strName）及成员函数eat()
+//士兵类从人类派生，含有数据成员编号（m_strCode)及成员函数attack()
+//在main函数通过对数据的访问，体会公有继承的语法特点。
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+/**
+ * 定义人的类: Person
+ * 数据成员姓名: m_strName
+ * 成员函数: eat()
+ */
+class Person
+{
+public:
+    string m_strName;
+	void eat()
+	{
+		cout << "eat" << endl;
+	}
+};
+
+/**
+ * 定义士兵类: Soldier
+ * 士兵类公有继承人类: public
+ * 数据成员编号: m_strCode
+ * 成员函数: attack()
+ */
+class Soldier:public Person
+{
+public:
+	string m_strCode;
+	void attack()
+	{
+		cout << "fire!!!" << endl;
+	}
+};
+
+int main(void)
+{
+    // 创建Soldier对象
+	Soldier soldier;
+    // 给对象属性赋值
+    soldier.m_strName = "Jim";
+	soldier.m_strCode = "592";
+    // 打印对象属性值
+	cout << soldier.m_strName << endl;
+	cout << soldier.m_strCode << endl;
+    // 调用对象方法
+	soldier.eat();
+	soldier.attack();
+	
+
+	return 0;
+}
