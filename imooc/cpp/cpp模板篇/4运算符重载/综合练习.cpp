eb607ed8abadbd96fce27df7cf184f6033d0cec3
Gzmbw <unbroken1127@gmail.com>
2017-09-17 15:02:21 +0800

    练习题

---
+++ imooc/cpp/cpp模板篇/4运算符重载/综合练习.cpp
@@ -0,0 +1,67 @@
+/*
+定义Coordinate类
+数据成员：m_iX, m_iY
+成员函数：构造函数
+重载“--”运算符，重载“+”运算符
+*/
+#include <iostream>
+using namespace std;
+
+/**
+ * 定义Coordinate类
+ * 数据成员：m_iX，m_iY
+ * 成员函数：构造函数
+ * 重载--运算符，重载+运算符
+ */
+class Coordinate
+{
+public:
+    Coordinate(int x, int y)
+	{
+		m_iX = x;
+		m_iY = y;
+	}
+    // 前置--运算符重载
+	Coordinate &operator--()
+	{
+	    m_iX--;
+	    m_iY--;
+	    return *this;
+	}
+    // 后置--运算符重载
+    Coordinate operator--(int)
+    {
+        Coordinate old(*this);
+        m_iX--;
+        m_iY--;
+        return old;
+    }
+    // +号运算符重载
+    Coordinate operator+(const Coordinate &coor)
+    {
+        Coordinate temp(0,0);//注意这里要赋初始值
+        temp.m_iX=this->m_iX+coor.m_iX;
+        temp.m_iY=this->m_iY+coor.m_iY;
+        return temp;
+    }
+public:
+	int m_iX;
+	int m_iY;
+};
+
+int main(void)
+{
+	Coordinate coor1(1, 3);
+	Coordinate coor2(2, 4);
+	Coordinate coor3(0, 0);
+
+	coor1--;
+	--coor2;
+	coor3 = coor1 + coor2;
+
+	cout << coor3.m_iX << endl;
+	cout << coor3.m_iY << endl;
+
+	return 0;
+}
+
